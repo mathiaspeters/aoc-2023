be7eee2ff9bcaa3be4a30ebee62a201c487a3c48
Mathias Peters <mathias.peters6@gmail.com>
2023-12-07 12:13:10 +0100

    Solution for day 6

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -4,11 +4,41 @@ pub fn day6() {
 }
 
 pub fn part1() -> usize {
-    raw_input().len()
+    let lines = raw_input()
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .filter_map(|s| s.parse::<f64>().ok())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    lines[0]
+        .iter()
+        .zip(lines[1].iter())
+        .map(|(time, distance)| calculate(*time, *distance))
+        .fold(1, |acc, options| acc * options)
 }
 
 pub fn part2() -> usize {
-    raw_input().len()
+    let lines = raw_input()
+        .lines()
+        .map(|line| {
+            line.replace(
+                |c: char| c.is_whitespace() || c.is_alphabetic() || c == ':',
+                "",
+            )
+            .parse::<f64>()
+            .unwrap()
+        })
+        .collect::<Vec<_>>();
+    calculate(lines[0], lines[1])
+}
+
+fn calculate(time: f64, distance: f64) -> usize {
+    let temp = (time * time - 4.0 * (distance + 1.0)).sqrt();
+    let low = ((time - temp) / 2.0).ceil() as usize;
+    let high = ((time + temp) / 2.0).floor() as usize;
+    high - low + 1
 }
 
 #[cfg(not(test))]
@@ -27,11 +57,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1());
+        assert_eq!(288, part1());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2());
+        assert_eq!(71503, part2());
     }
 }
