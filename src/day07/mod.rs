f1060d391a251e23abfa98324c6f539eaaa9701e
Mathias Peters <mathias.peters6@gmail.com>
2023-12-09 09:12:24 +0100

    Solution for day 7

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -4,11 +4,142 @@ pub fn day7() {
 }
 
 pub fn part1() -> usize {
-    raw_input().len()
+    let mut hands = get_hands(false);
+    hands.sort_by(
+        |l, r| match l.get_type().partial_cmp(&r.get_type()).unwrap() {
+            std::cmp::Ordering::Equal => l.cards.partial_cmp(&r.cards).unwrap(),
+            ord @ _ => ord,
+        },
+    );
+    hands
+        .into_iter()
+        .enumerate()
+        .fold(0, |acc, (i, Hand { bid, .. })| acc + (bid * (i + 1)))
 }
 
 pub fn part2() -> usize {
-    raw_input().len()
+    let mut hands = get_hands(true);
+    hands.sort_by(|l, r| {
+        match l
+            .get_type_with_jokers()
+            .partial_cmp(&r.get_type_with_jokers())
+            .unwrap()
+        {
+            std::cmp::Ordering::Equal => l.cards.partial_cmp(&r.cards).unwrap(),
+            ord @ _ => ord,
+        }
+    });
+    hands
+        .into_iter()
+        .enumerate()
+        .fold(0, |acc, (i, Hand { bid, .. })| acc + (bid * (i + 1)))
+}
+
+fn get_hands(joker: bool) -> Vec<Hand> {
+    raw_input()
+        .lines()
+        .map(|s| {
+            let mut parts = s.split_ascii_whitespace();
+            let cards = parts
+                .next()
+                .unwrap()
+                .chars()
+                .map(|c: char| match c {
+                    'T' => 10_u8,
+                    'J' => match joker {
+                        true => 0_u8,
+                        false => 11_u8,
+                    },
+                    'Q' => 12_u8,
+                    'K' => 13_u8,
+                    'A' => 14_u8,
+                    _ => c.to_digit(10).unwrap() as u8,
+                })
+                .collect();
+            let bid = parts.next().unwrap().parse::<usize>().unwrap();
+            Hand { cards, bid }
+        })
+        .collect::<Vec<_>>()
+}
+
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<u8>,
+    bid: usize,
+}
+
+impl Hand {
+    fn get_type(&self) -> HandType {
+        let mut counts = [0_u8; 15];
+        for val in self.cards.iter() {
+            counts[*val as usize] += 1;
+        }
+        counts.sort_unstable();
+        counts.reverse();
+        if counts[0] == 5 {
+            HandType::FiveOfAKind
+        } else if counts[0] == 4 {
+            HandType::FourOfAKind
+        } else if counts[0] == 3 {
+            if counts[1] == 2 {
+                HandType::FullHouse
+            } else {
+                HandType::ThreeOfAKind
+            }
+        } else if counts[0] == 2 {
+            if counts[1] == 2 {
+                HandType::TwoPair
+            } else {
+                HandType::OnePair
+            }
+        } else {
+            HandType::HighCard
+        }
+    }
+
+    fn get_type_with_jokers(&self) -> HandType {
+        let mut jokers = 0;
+        let mut counts = [0_u8; 15];
+        for val in self.cards.iter() {
+            if *val == 0 {
+                jokers += 1;
+            } else {
+                counts[*val as usize] += 1;
+            }
+        }
+        counts.sort_unstable();
+        counts.reverse();
+        if counts[0] + jokers == 5 {
+            HandType::FiveOfAKind
+        } else if counts[0] + jokers == 4 {
+            HandType::FourOfAKind
+        } else if counts[0] + jokers == 3 {
+            if counts[1] == 2 {
+                HandType::FullHouse
+            } else {
+                HandType::ThreeOfAKind
+            }
+        } else if counts[0] + jokers == 2 {
+            if counts[1] == 2 {
+                HandType::TwoPair
+            } else {
+                HandType::OnePair
+            }
+        } else {
+            HandType::HighCard
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
 }
 
 #[cfg(not(test))]
@@ -27,11 +158,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1());
+        assert_eq!(6440, part1());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2());
+        assert_eq!(5905, part2());
     }
 }
